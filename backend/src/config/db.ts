eba1442a28827d2a7e3e085e7c043afa03d4069f
Tushar Chakravarti <tusharch040920052gmail.com>
2026-08-08 21:27:37 +0530

    first commit

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
+import dns from "dns";
 
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 export const connectDB = async (): Promise<typeof mongoose> => {
   const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/loan_management_db';
 
